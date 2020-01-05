f24e8b358d71a85eb167c0a5769596617bd1cf27
Lukious <zhsjzhsj@gmail.com>
2020-01-05 23:08:13 +0900

    Numpy Preprocessing is DONE!

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,11 @@
+'''
+Code By Lukious
+Memvber of BMCL
+
+github.com/Lukious
+'''
 from PIL import Image
+import numpy
 import os,glob
 
 folder_path = './pokemon-dataset/images/images'
@@ -15,6 +22,13 @@ def JPG_case(filename):
     im.thumbnail(size)  
     im.save('./pokemon-dataset/images/Preprocessed/'+filename[:-4]+'.png')
 
+def Image2Matrix(filename):
+    im = Image.open(folder_path+'/'+filename)
+    size = (28, 28)
+    im.thumbnail(size)  
+    ImageMatrix = numpy.asarray(im.convert('L'))
+    numpy.save('./pokemon-dataset/images/MatrixPreprocessed/'+filename[:-4],ImageMatrix)
+
 
 for filename in os.listdir(folder_path):
     print(filename)
@@ -22,3 +36,4 @@ for filename in os.listdir(folder_path):
         PNG_case(filename)
     elif(filename[-3:] == 'jpg'):
         JPG_case(filename)
+    Image2Matrix(filename)
